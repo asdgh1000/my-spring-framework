d9da614fbcd20ae1f917f99d0bc16e0d4681d0d1
tianwei.li <tianwei.li@bitmart.com>
2019-09-19 11:33:47 +0800

    add proxyFactory2

--- spring-myproject/src/main/java/com/test/proxy/dynamic/jdk/ProxyFactory2.java
+++ spring-myproject/src/main/java/com/test/proxy/dynamic/jdk/ProxyFactory2.java
@@ -20,19 +20,12 @@ import java.lang.reflect.Proxy;
  * @author ltw
  * on 2019-09-18.
  */
-public class ProxyFactory implements InvocationHandler {
-
-	private Class<?> target;
+public class ProxyFactory2 implements InvocationHandler {
 
 	private Object real;
 
-//	public ProxyFactory(Class<?> target) {
-//		this.target = target;
-//	}
-
-	public Object bind(Object real) {
+	public ProxyFactory2(Object real) {
 		this.real = real;
-		return Proxy.newProxyInstance(real.getClass().getClassLoader(), real.getClass().getInterfaces(), this);
 	}
 
 	@Override
@@ -47,9 +40,33 @@ public class ProxyFactory implements InvocationHandler {
 		// 设置变量可以保存动态代理类，默认名称以 $Proxy0 格式命名
 		System.getProperties().setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
 
-		Calculator proxy = (Calculator) new ProxyFactory().bind(new Calculator.CalculatorImpl());
+		// 1. 创建被代理的对象，UserService接口的实现类
+		Calculator calculatorImpl = new Calculator.CalculatorImpl();
+
+		// 2. 获取对应的 ClassLoader
+		ClassLoader classLoader = calculatorImpl.getClass().getClassLoader();
+
+		// 3. 获取所有接口的Class，这里的UserServiceImpl只实现了一个接口UserService，
+		Class[] interfacces = calculatorImpl.getClass().getInterfaces();
+
+		// 4. 创建一个将传给代理类的调用请求处理器，处理所有的代理对象上的方法调用
+		//     这里创建的是一个自定义的日志处理器，须传入实际的执行对象 userServiceImpl
+		InvocationHandler proxyFactory2 = new ProxyFactory2(calculatorImpl);
+
+		   /*
+		   5.根据上面提供的信息，创建代理对象 在这个过程中，
+               a.JDK会通过根据传入的参数信息动态地在内存中创建和.class 文件等同的字节码
+               b.然后根据相应的字节码转换成对应的class，
+               c.然后调用newInstance()创建代理实例
+		 */
+		Calculator proxy = (Calculator) Proxy.newProxyInstance(classLoader, interfacces, proxyFactory2);
+
 		proxy.add(1, 2);
+
+		// 保存JDK动态代理生成的代理类，类名保存为 UserServiceProxy
+		ProxyUtils.generateClassFile(calculatorImpl.getClass(), "calculateProxy");
 	}
+
 
 	private void before() {
 		System.out.println("jdk dynamic proxy start");
